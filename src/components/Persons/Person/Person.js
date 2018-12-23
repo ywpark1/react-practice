7529de15e547a6aa3b70c2f739384a916df82fed
Yeonwoo Park <ywoo1234@gmail.com>
2018-12-23 15:54:39 -0500

    Add forwardRef

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -21,6 +21,7 @@ class Person extends Component {
   constructor(props) {
     super(props);
     console.log('[Person.js] Inside Constructor', props);
+    this.inputElement = React.createRef();
   }
 
   componentWillMount() {
@@ -30,9 +31,14 @@ class Person extends Component {
   componentDidMount() {
     console.log('[Person.js] Inside componentDidMount()');
     if (this.props.position === 0) {
-      this.inputElement.focus();
+      this.inputElement.current.focus();
     }
   }
+
+  focus() {
+    this.inputElement.current.focus();
+  }
+
   render() {
     console.log('[Person.js] Inside render()');
     return (
@@ -42,9 +48,7 @@ class Person extends Component {
         </p>
         <p>{this.props.children}</p>
         <input
-          ref={input => {
-            this.inputElement = input;
-          }}
+          ref={this.inputElement}
           type="text"
           onChange={this.props.changed}
           value={this.props.name}
--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -21,6 +21,7 @@ class Persons extends PureComponent {
   constructor(props) {
     super(props);
     console.log('[Persons.js] Inside Constructor', props);
+    this.lastPersonRef = React.createRef();
   }
 
   componentWillMount() {
@@ -29,6 +30,7 @@ class Persons extends PureComponent {
 
   componentDidMount() {
     console.log('[Persons.js] Inside componentDidMount()');
+    this.lastPersonRef.current.focus();
   }
 
   componentWillReceiveProps(nextProps) {
@@ -74,6 +76,7 @@ class Persons extends PureComponent {
           name={person.name}
           position={index}
           age={person.age}
+          ref={this.lastPersonRef}
           key={person.id}
           changed={event => this.props.changed(event, person.id)}
         />

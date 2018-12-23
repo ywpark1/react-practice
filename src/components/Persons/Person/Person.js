4cd1d06b4f3b7c364a1c1fc5986923fa5b95386c
Yeonwoo Park <ywoo1234@gmail.com>
2018-12-23 13:33:49 -0500

    Update to  stateful components

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,30 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './Person.css';
 
-// class Person extends Component {
-//   render() {
-//     return (
-//       <div>
-//         <p>
-//           I'm {this.props.name} and I am {this.props.age} year old!
-//         </p>
-//         <p>{this.props.children}</p>
-//       </div>
-//     );
-//   }
-// }
+// const person = props => {
+//   return (
+//     <div className={classes.Person}>
+//       <p onClick={props.click}>
+//         I'm {props.name} and I am {props.age} year old!
+//       </p>
+//       <p>{props.children}</p>
+//       <input type="text" onChange={props.changed} value={props.name} />
+//     </div>
+//   );
+// };
 
-const person = props => {
-  return (
-    <div className={classes.Person}>
-      <p onClick={props.click}>
-        I'm {props.name} and I am {props.age} year old!
-      </p>
-      <p>{props.children}</p>
-      <input type="text" onChange={props.changed} value={props.name} />
-    </div>
-  );
-};
+// export default person;
 
-// export default Person;
-export default person;
+class Person extends Component {
+  render() {
+    return (
+      <div className={classes.Person}>
+        <p onClick={this.props.click}>
+          I'm {this.props.name} and I am {this.props.age} year old!
+        </p>
+        <p>{this.props.children}</p>
+        <input
+          type="text"
+          onChange={this.props.changed}
+          value={this.props.name}
+        />
+      </div>
+    );
+  }
+}
+
+export default Person;
